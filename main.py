9248b480231ab21ac5d072c50a4ac57268b4d9dc
Леонид Петров <leonidpetrov637@gmail.com>
2024-04-12 16:45:37 +0300

    added a function for receiving vacancies from transferred companies

--- main.py
+++ main.py
@@ -3,16 +3,27 @@ from src.utils import get_hh_data, create_database, save_data_to_database
 
 
 def main():
-    api_hh_ru = 'https://api.hh.ru/vacancies'
+    api_hh_ru = 'https://api.hh.ru/vacancies' # 'https://api.hh.ru/employers'
     company_ids = [
-        'HeadHunter'
+        '1473866', # Сбербанк-Сервис 1.
+        '4023', # Райффайзен Банк 2.
+        '205', # Марс 3.
+        '4219', # Tele2 4.
+        '23040', # Банк Открытие 5.
+        '740', # Норникель 6.
+        '3838', # Компания ПЭК 7.
+        '1057', # Лаборатория Касперского 8.
+        '1180', # Bayer 9.
+        '208707', # ВсеИнструменты.ру 10.
+        '1740' # Яндекс 11.
     ]
 
     params = config()
 
     data = get_hh_data(api_hh_ru, company_ids)
-    create_database('hh_data', params)
-    save_data_to_database(data, 'hh_data', params)
+    print(len(data))
+    # create_database('hh_data', params)
+    # save_data_to_database(data, 'hh_data', params)
 
 
 if __name__ == '__main__':
